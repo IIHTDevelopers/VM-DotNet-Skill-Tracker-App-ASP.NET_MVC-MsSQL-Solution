86fbc11f4e02071de8a9f960329a4b6e06b8c593
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-02 12:52:31 +0530

    Add files via upload

---
+++ SkillTrackerApp/DAL/Repository/SkillTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using SkillTrackerApp.DAL.Interface;
+using SkillTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkillTrackerApp.DAL.Repository
+{
+    public class SkillTrackerSkill : ISkillTrackerInterface
+    {
+        private ISkillTrackerRepository _repo;
+        public SkillTrackerSkill(ISkillTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteSkill(int SkillId)
+        {
+            var res= _repo.DeleteSkill(SkillId);
+            return res;
+        }
+
+        public Skill GetSkillByID(int SkillId)
+        {
+            return _repo.GetSkillByID(SkillId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        IEnumerable<Skill> ISkillTrackerInterface.GetSkills()
+        {
+            return _repo.GetSkills();
+        }
+
+        Skill ISkillTrackerInterface.InsertSkill(Skill Skill)
+        {
+            return _repo.InsertSkill(Skill);
+        }
+
+        bool ISkillTrackerInterface.UpdateSkill(Skill Skill)
+        {
+            return _repo.UpdateSkill(Skill);
+        }
+    }
+}
